3cd6898971544e95b0ead167ab7374a67e728e4f
haripriya guthikonda <bluecloud.priya@gmail.com>
2019-09-17 22:34:43 +0530

    update

--- connection/connection.js
+++ connection/connection.js
@@ -2,10 +2,10 @@ var sql = require("mssql");
 var connect = function()
 {
     var conn = new sql.ConnectionPool({
-        user: 'sa',
-        password: 'priya@6468',
-        server: 'DESKTOP-6JTOD35',
-        database: 'HARIPRIYA'
+        user: 'username',
+        password: '12345',
+        server: 'servername',
+        database: 'testdb'
     });
  
     return conn;
